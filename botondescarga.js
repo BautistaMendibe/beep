c06e46a2952f737df6ee2cee5b15cfd0c668a318
bautista <bautistamendibe10@gmail.com>
2018-12-14 22:17:38 -0300

    primer commit

---
+++ botondescarga.js
@@ -0,0 +1,12 @@
+
+function mostrar() {
+
+	document.getElementById("cajaformulario").style.display="block";
+
+	document.getElementById("caja1").style.display="none";
+	document.getElementById("caja2").style.display="none";
+	document.getElementById("caja3").style.display="none";
+	
+	
+}
+
---
+++ enviar.php
@@ -0,0 +1,14 @@
+<?php
+	
+	$destino="companybeep@gmail.com";
+	$nombre=$_POST["nombre"];
+	$correo=$_POST["correo"];
+	$mensaje=$_POST["mensaje"];
+
+	$contenido="Nombre: ".$nombre . "\nCorreo: ".$correo. "\nMensaje= ".$mensaje; 
+
+	mail($destino, "Contacto", $contenido); 
+	header("Location: paginacompartir.html"); 
+
+
+?>
